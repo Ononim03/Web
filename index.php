ce57aacb5aefe345496cd3134cea0d45d7a38688
SLAVACOM <slavacom121@gmail.com>
2022-10-07 17:03:44 +0300

    multiplication table

--- index.php
+++ index.php
@@ -1,7 +1,15 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>homework№1</title>
+    <link rel="stylesheet" href="style.css"/>
+    <link rel="icon" href="https://sun9-16.userapi.com/impg/2q1bniJWyu_N8rqZBnNaYFZKpWUkbjwWYXN05A/_gV4DVNgvrI.jpg?size=1620x2160&quality=96&sign=2ca2a0cbdbed68538f461bcc43417720&type=album"/>
+</head>
 <pre>
+
     <?php
         echo "<table>";
+        echo "<caption>Multiplication Table</caption>";
         for ($i=1;$i<11;$i++){
             echo "<tr>";
             for ($j=1;$j<11;$j++){
@@ -10,3 +18,6 @@
              echo "</tr>";
         }
         echo "</table>";
+    ?></pre>
+</html>
+    
