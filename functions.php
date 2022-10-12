3ffc33af41c5bffe845ca3a80553bf8da0190812
ononim <tzinatullin16@gmail.com>
2022-10-12 18:51:24 +0300

    classwork

---
+++ functions.php
@@ -0,0 +1,7 @@
+<?php
+    function find_min($x1, $x2, $x3 = 2e9, $x4 = 2e9, $x5 = 2e9) {
+        $m12 = $x1 < $x2 ? $x1 : $x2;
+        $m34 = $x3 < $x4 ? $x3 : $x4;
+        $m14 = $m12 < $m34 ? $m12 : $m34;
+        return $m14 < $x5 ? $m14 : $x5;
+    }
--- index.php
+++ index.php
@@ -1,23 +1,38 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>homework№1</title>
-    <link rel="stylesheet" href="style.css"/>
-    <link rel="icon" href="https://sun9-16.userapi.com/impg/2q1bniJWyu_N8rqZBnNaYFZKpWUkbjwWYXN05A/_gV4DVNgvrI.jpg?size=1620x2160&quality=96&sign=2ca2a0cbdbed68538f461bcc43417720&type=album"/>
-</head>
-<pre>
+<?php
 
-    <?php
-        echo "<table>";
-        echo "<caption>Multiplication Table</caption>";
-        for ($i=1;$i<11;$i++){
-            echo "<tr>";
-            for ($j=1;$j<11;$j++){
-                echo "<td>" . $i * $j . "</td>";
-            }
-             echo "</tr>";
-        }
-        echo "</table>";
-    ?></pre>
-</html>
-    
+require_once "./functions.php";
+
+echo find_min(1, 29) . PHP_EOL;
+echo find_min(random_int(1, 56),random_int(1, 56),random_int(1, 56),random_int(1, 56),random_int(1, 56));
+
+$id = 1;
+$link = "https://vk.com/im?rel=" . $id;
+$myHtml = '<a href="' . $link . '">Chat</a>';
+$myHtml = <<<HTML
+    <a target="_blank" href="$link">Chat</a>
+HTML;
+//HEREDOC
+echo $myHtml . PHP_EOL;
+echo str_pad(12, 5, 0, STR_PAD_LEFT);
+$pos =  strpos("Mom", "Mom");
+if ($pos === false ) {
+    echo "Не Нашлось";
+}
+
+
+$arr = [1,2,3,4,5,"i1" => 'word1',"i2" => "word2", "i3" => 'word3', 100, true];
+var_dump('<pre>', $arr);
+if (isset($arr[5])) {
+    var_dump("существует");
+}
+
+
+
+
+
+$x = &$arr['челек'];
+if ($x == null) {
+    $x = 10;
+}
+error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING ^ E_ERROR); # ошибки идут в жопу
+error_reporting(E_ALL); # Возврат ошибок
