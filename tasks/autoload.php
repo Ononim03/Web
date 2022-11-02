9044f359ab38f7139b61568543bc3b0931ef89ac
Ononim03 <93390852+Ononim03@users.noreply.github.com>
2022-11-02 15:08:04 +0300

    4

---
+++ tasks/autoload.php
@@ -0,0 +1,5 @@
+<?php
+require_once 'task1/autoload.php';
+require_once 'task2/autoload.php';
+require_once 'task3/autoload.php';
+require_once 'task4/autoload.php';
--- tasks/create_autoload.php
+++ tasks/create_autoload.php
@@ -1,17 +1,11 @@
 <?php
 
-$filelist = glob("*", GLOB_ONLYDIR);
-$main_str = "<?php \n";
-foreach ($filelist as $dir) {
-    $str =  "<?php \n";
-    $pattern = $dir . '/' . '*.php';
-    var_dump(glob($pattern));
-    foreach (glob($pattern) as $php_class ){
-        $php_class_end = explode( '/', $php_class);
-        $php_class_end = $php_class_end[count($php_class_end)- 1];
-        $str .= "require_once('$php_class_end');\n";
-    }
-    file_put_contents($dir . '/' . 'autoload.php', $str);
-    $main_str .= "require_once('$dir/autoload.php');\n";
+$dirs_list = glob('*', GLOB_ONLYDIR);
+file_put_contents('autoload.php', "<?php\n");
+foreach ($dirs_list as $dir) {
+    $file_name = $dir . '/autoload.php';
+
+    file_put_contents($file_name, file_get_contents('../autoload.php'));
+
+    file_put_contents('autoload.php', "require_once '$dir/autoload.php';\n", FILE_APPEND);
 }
-file_put_contents('autoload.php', $main_str);
